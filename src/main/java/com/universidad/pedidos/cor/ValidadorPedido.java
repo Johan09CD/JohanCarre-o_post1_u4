46e844d13f2b0127a063691f43d913be0ca1345d
JohanStevenCarreñoDaza <carrenojohan52@gmail.com>
2026-03-23 16:02:18 -0500

    feat: implementación patrón Chain of Responsibility

--- src/main/java/com/universidad/pedidos/cor/ValidadorPedido.java
+++ src/main/java/com/universidad/pedidos/cor/ValidadorPedido.java
@@ -1,5 +1,22 @@
 package com.universidad.pedidos.cor;
 
-public class ValidadorPedido {
-    
+import com.universidad.pedidos.modelo.Pedido;
+
+public abstract class ValidadorPedido {
+
+    protected ValidadorPedido siguiente;
+
+    public ValidadorPedido setNext(ValidadorPedido next) {
+        this.siguiente = next;
+        return next;
+    }
+
+    public abstract boolean validar(Pedido pedido);
+
+    protected boolean delegar(Pedido pedido) {
+        if (siguiente == null) {
+            return true;
+        }
+        return siguiente.validar(pedido);
+    }
 }
